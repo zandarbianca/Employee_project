0278bbaf6fc2cca6abd2ff5235cfdac53ca7c296
Bianca Zandar <zandar.bianca@gmail.com>
2021-08-03 18:30:00 +0300

    Initial commit

---
+++ src/main/java/tech.getarrays.employeemanager/model/Employee.java
@@ -0,0 +1,73 @@
+package tech.getarrays.employeemanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    public Employee() { }
+
+    public Employee(Long id, String name, String email, String jobTitle, String phone, String imageUrl, String employeeCode) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getJobTitle() { return jobTitle; }
+
+    public void setJobTitle(String jobTitle) { this.jobTitle = jobTitle; }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getImageUrl() { return imageUrl; }
+
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+
+    public String getEmployeeCode() { return employeeCode; }
+
+    public void setEmployeeCode(String employeeCode) { this.employeeCode = employeeCode; }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", jobTitle='").append(jobTitle).append('\'');
+        sb.append(", phone='").append(phone).append('\'');
+        sb.append(", imageUrl='").append(imageUrl).append('\'');
+        sb.append(", employeeCode='").append(employeeCode).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
